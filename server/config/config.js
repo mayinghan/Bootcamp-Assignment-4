f7528f624136ab0e9b1ae389650444608844e4d7
mayinghan <ma.yingha@ufl.edu>
2018-09-25 01:31:33 -0400

    changed port

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   db: {
     uri: 'mongodb://CEN3031:CEN3031TA@ds035503.mlab.com:35503/ufwebapp', //place the URI of your mongo database here.
   }, 
-  port: 8080
+  port: process.env.PORT || 8080
 };
